53e939c94c7ae17d10d84b53f8604d5da2d672c4
Pratik <pratiksahoo3551@users.noreply.github.com>
2026-03-11 21:41:02 +0530

    Update path in run_dashboard.bat

--- trial_run/symptoms/run_dashboard.bat
+++ trial_run/symptoms/run_dashboard.bat
@@ -3,7 +3,7 @@ echo Starting the COVID-19 Symptoms Knowledge Flow Dashboard...
 echo Make sure you have installed the requirements using: pip install -r requirements.txt
 echo.
 
-cd trial_run\symptoms\notebooks
+cd notebooks
 python -m streamlit run step6_dashboard.py
 
 pause

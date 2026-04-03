741a7d51db53b78f722ddb211cb2f710bad2ad4a
anshita-sharma-iiserp <anshita.sharma@students.iiserpune.ac.in>
2026-04-04 00:06:45 +0530

    Delete trial_run/symptoms directory

--- trial_run/symptoms/run_dashboard.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting the COVID-19 Symptoms Knowledge Flow Dashboard...
-echo Make sure you have installed the requirements using: pip install -r requirements.txt
-echo.
-
-cd notebooks
-python -m streamlit run step6_dashboard.py
-
-pause
